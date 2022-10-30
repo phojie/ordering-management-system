b5437c4ad5a6637c0a89219c7a50c54141278af2
phojie rengel <phojrengel@gmail.com>
2022-10-31 01:29:21 +0800

    chore: set autoimports to template to false

--- vite.config.ts
+++ vite.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
 
       // Auto import inside Vue template
       // see https://github.com/unjs/unimport/pull/15 and https://github.com/unjs/unimport/pull/72
-      vueTemplate: true,
+      vueTemplate: false,
 
       // see https://github.com/antfu/unplugin-auto-import/pull/23/
       resolvers: [
