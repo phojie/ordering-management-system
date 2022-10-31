f241f154307b80932c69736d4f859d9f02c9824a
phojie rengel <phojrengel@gmail.com>
2022-10-31 17:32:34 +0800

    feat(components): add j-spinner component to j-btn

---
+++ resources/js/j-components/JBtn.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+interface Props {
+  // string
+  id?: string
+  name?: string
+  type?: 'submit' | 'reset' | 'button'
+  label?: string
+
+  // boolean
+  isDirty?: boolean
+  isDisabled?: boolean
+  isReadOnly?: boolean
+  isLoading?: boolean
+
+  // object
+}
+
+// set default props
+withDefaults(defineProps<Props>(), {
+  type: 'button',
+})
+</script>
+
+<template>
+  <button
+    :type="type"
+    :readonly="isReadOnly"
+    :disabled="isDisabled"
+    class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+  >
+    <span v-if="!isLoading">
+      <!-- label area -->
+      {{ label }}
+      <template v-if="label">
+        <slot />
+      </template>
+    </span>
+
+    <span v-if="isLoading" class="flex space-x-2">
+      <!-- spinner area -->
+      <JSpinner class="w-5 h-5 " />
+      <span>
+        Processing...
+      </span>
+    </span>
+  </button>
+</template>
---
+++ resources/js/j-components/JSpinner.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <IcomoonFreeSpinner2 class="animate-spin" />
+</template>
