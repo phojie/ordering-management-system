6a49d304dc154977ce9081621a1ea241b1197b52
phojie rengel <phojrengel@gmail.com>
2022-11-02 03:05:30 +0800

    style: linter bot

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -13,19 +13,24 @@ defineProps({
   >
     <div v-if="canLogin" class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
       <Link
-        v-if="$page.props.auth.user" :href="route('dashboard')"
+        v-if="$page.props.auth.user"
+        :href="route('dashboard')"
         class="text-sm text-gray-700 underline dark:text-gray-500"
       >
         Dashboard
       </Link>
 
       <template v-else>
-        <Link :href="route('login')" class="text-sm text-gray-700 underline dark:text-gray-500">
+        <Link
+          :href="route('login')"
+          class="text-sm text-gray-700 underline dark:text-gray-500"
+        >
           Log in
         </Link>
 
         <Link
-          v-if="canRegister" :href="route('register')"
+          v-if="canRegister"
+          :href="route('register')"
           class="ml-4 text-sm text-gray-700 underline dark:text-gray-500"
         >
           Register
