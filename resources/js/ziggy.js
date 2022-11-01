9ea230842f4e386a6fc5b4da7693dac2bb17c8ae
phojie rengel <phojrengel@gmail.com>
2022-11-02 03:05:08 +0800

    chore: ziggy routes config

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,4 +1,25 @@
-const Ziggy = { url: 'http:\/\/rms.test', port: null, defaults: {}, routes: { 'ignition.healthCheck': { uri: '_ignition\/health-check', methods: ['GET', 'HEAD'] }, 'ignition.executeSolution': { uri: '_ignition\/execute-solution', methods: ['POST'] }, 'ignition.updateConfig': { uri: '_ignition\/update-config', methods: ['POST'] }, 'dashboard': { uri: 'dashboard', methods: ['GET', 'HEAD'] }, 'register': { uri: 'register', methods: ['GET', 'HEAD'] }, 'login': { uri: 'login', methods: ['GET', 'HEAD'] }, 'password.request': { uri: 'forgot-password', methods: ['GET', 'HEAD'] }, 'password.email': { uri: 'forgot-password', methods: ['POST'] }, 'password.reset': { uri: 'reset-password\/{token}', methods: ['GET', 'HEAD'] }, 'password.update': { uri: 'reset-password', methods: ['POST'] }, 'verification.notice': { uri: 'verify-email', methods: ['GET', 'HEAD'] }, 'verification.verify': { uri: 'verify-email\/{id}\/{hash}', methods: ['GET', 'HEAD'] }, 'verification.send': { uri: 'email\/verification-notification', methods: ['POST'] }, 'password.confirm': { uri: 'confirm-password', methods: ['GET', 'HEAD'] }, 'logout': { uri: 'logout', methods: ['POST'] } } }
+const Ziggy = {
+  url: 'http:\/\/rms.test',
+  port: null,
+  defaults: {},
+  routes: {
+    // 'ignition.healthCheck': { uri: '_ignition\/health-check', methods: ['GET', 'HEAD'] },
+    // 'ignition.executeSolution': { uri: '_ignition\/execute-solution', methods: ['POST'] },
+    // 'ignition.updateConfig': { uri: '_ignition\/update-config', methods: ['POST'] },
+    // 'register': { uri: 'register', methods: ['GET', 'HEAD'] },
+    // 'login': { uri: 'login', methods: ['GET', 'HEAD'] },
+    // 'password.request': { uri: 'forgot-password', methods: ['GET', 'HEAD'] },
+    // 'password.email': { uri: 'forgot-password', methods: ['POST'] },
+    // 'password.reset': { uri: 'reset-password\/{token}', methods: ['GET', 'HEAD'] },
+    // 'password.update': { uri: 'reset-password', methods: ['POST'] },
+    // 'verification.notice': { uri: 'verify-email', methods: ['GET', 'HEAD'] },
+    // 'verification.verify': { uri: 'verify-email\/{id}\/{hash}', methods: ['GET', 'HEAD'] },
+    // 'verification.send': { uri: 'email\/verification-notification', methods: ['POST'] },
+    // 'password.confirm': { uri: 'confirm-password', methods: ['GET', 'HEAD'] },
+    // 'logout': { uri: 'logout', methods: ['POST'] },
+    // 'dashboard': { uri: 'dashboard', methods: ['POST'] },
+  },
+}
 
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined')
   Object.assign(Ziggy.routes, window.Ziggy.routes)
