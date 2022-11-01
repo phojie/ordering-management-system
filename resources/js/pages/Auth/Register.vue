2ec525f5081902e844860cc2145e4dfbdc82ebea
phojie rengel <phojrengel@gmail.com>
2022-11-02 03:40:51 +0800

    refactor: remove some unused lines

--- resources/js/pages/Auth/Register.vue
+++ resources/js/pages/Auth/Register.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3'
 const form = useForm({
-  name: '',
+  username: '',
   email: '',
   password: '',
   password_confirmation: '',
@@ -16,43 +15,41 @@ const submit = () => {
 </script>
 
 <template>
-  <GuestLayout>
-    <Head title="Register" />
+  <Head title="Register" />
 
-    <form @submit.prevent="submit">
-      <div>
-        <InputLabel for="name" value="Name" />
-        <TextInput id="name" v-model="form.name" type="text" class="block w-full mt-1" required autofocus autocomplete="name" />
-        <InputError class="mt-2" :message="form.errors.name" />
-      </div>
+  <form @submit.prevent="submit">
+    <div>
+      <InputLabel for="username" value="Name" />
+      <TextInput id="username" v-model="form.username" type="text" class="block w-full mt-1" required autofocus autocomplete="username" />
+      <InputError class="mt-2" :message="form.errors.username" />
+    </div>
 
-      <div class="mt-4">
-        <InputLabel for="email" value="Email" />
-        <TextInput id="email" v-model="form.email" type="email" class="block w-full mt-1" required autocomplete="username" />
-        <InputError class="mt-2" :message="form.errors.email" />
-      </div>
+    <div class="mt-4">
+      <InputLabel for="email" value="Email" />
+      <TextInput id="email" v-model="form.email" type="email" class="block w-full mt-1" required autocomplete="username" />
+      <InputError class="mt-2" :message="form.errors.email" />
+    </div>
 
-      <div class="mt-4">
-        <InputLabel for="password" value="Password" />
-        <TextInput id="password" v-model="form.password" type="password" class="block w-full mt-1" required autocomplete="new-password" />
-        <InputError class="mt-2" :message="form.errors.password" />
-      </div>
+    <div class="mt-4">
+      <InputLabel for="password" value="Password" />
+      <TextInput id="password" v-model="form.password" type="password" class="block w-full mt-1" required autocomplete="new-password" />
+      <InputError class="mt-2" :message="form.errors.password" />
+    </div>
 
-      <div class="mt-4">
-        <InputLabel for="password_confirmation" value="Confirm Password" />
-        <TextInput id="password_confirmation" v-model="form.password_confirmation" type="password" class="block w-full mt-1" required autocomplete="new-password" />
-        <InputError class="mt-2" :message="form.errors.password_confirmation" />
-      </div>
+    <div class="mt-4">
+      <InputLabel for="password_confirmation" value="Confirm Password" />
+      <TextInput id="password_confirmation" v-model="form.password_confirmation" type="password" class="block w-full mt-1" required autocomplete="new-password" />
+      <InputError class="mt-2" :message="form.errors.password_confirmation" />
+    </div>
 
-      <div class="flex items-center justify-end mt-4">
-        <Link :href="route('login')" class="text-sm text-gray-600 underline hover:text-gray-900">
-          Already registered?
-        </Link>
+    <div class="flex items-center justify-end mt-4">
+      <Link :href="route('login')" class="text-sm text-gray-600 underline hover:text-gray-900">
+        Already registered?
+      </Link>
 
-        <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-          Register
-        </PrimaryButton>
-      </div>
-    </form>
-  </GuestLayout>
+      <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+        Register
+      </PrimaryButton>
+    </div>
+  </form>
 </template>
