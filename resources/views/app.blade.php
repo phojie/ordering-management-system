54b33d266cd2c4cdbf3e5111a0ae3fe757748a1c
phojie rengel <phojrengel@gmail.com>
2022-11-02 05:29:18 +0800

    style: linter bot

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <title inertia>{{ config('app.name', 'RS') }}</title> --}}
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <title inertia>{{ config('app.name', 'RS') }}</title> --}}
 
-        <!-- Scripts -->
-        @routes
-        @vite('resources/js/app.js')
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Scripts -->
+    @routes
+    @vite('resources/js/app.js')
+    @inertiaHead
+  </head>
+  <body class="font-sans antialiased">
+    @inertia
+  </body>
 </html>
