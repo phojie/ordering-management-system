00df2d458b9ac6daf4d0eb45c2a01ff04e2507b7
phojie rengel <phojrengel@gmail.com>
2022-11-02 15:28:51 +0800

    chore: app reconfiguration

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,8 @@ import { InertiaProgress } from '@inertiajs/progress'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createPinia } from 'pinia'
 import HighchartsVue from 'highcharts-vue'
-import Highcharts from 'highcharts'
+impHighcharts from 'highcharts'
 import exportingInit from 'highcharts/modules/exporting'
-// import { Ziggy } from '@/ziggy'
 import DefaultLayout from '@/layouts/Default.vue'
 
 const pinia = createPinia()
@@ -17,7 +16,7 @@ const appName = window.document.getElementsByTagName('title')[0]?.innerText || '
 
 createInertiaApp({
   title: title => `${title} - ${appName}`,
-  //   resolve: name => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+  // resolve: name => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
   resolve: (name) => {
     const page = resolvePageComponent(
             `./pages/${name}.vue`,
