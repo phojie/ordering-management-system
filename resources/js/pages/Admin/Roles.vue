d37099806b7069cb720443b9080ad8a138fcf50b
phojie rengel <phojrengel@gmail.com>
2022-11-02 11:54:44 +0800

    style: linter format

---
+++ resources/js/pages/Admin/Roles.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+}>()
+
+defineOptions({
+  layout: AdminLayout,
+})
+</script>
+
+<template>
+  <Head>
+    <title>Roles</title>
+  </Head>
+
+  <div>
+    Roles page
+  </div>
+</template>
