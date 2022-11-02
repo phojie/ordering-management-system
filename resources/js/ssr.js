215f7e7b2687932b70568349155b0f47d1e88753
phojie rengel <phojrengel@gmail.com>
2022-11-02 15:35:40 +0800

    perf: remove highcharts from ssr

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -3,10 +3,9 @@ import { renderToString } from '@vue/server-renderer'
 import { Head, createInertiaApp } from '@inertiajs/inertia-vue3'
 import createServer from '@inertiajs/server'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
-import HighchartsVue from 'highcharts-vue'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'RMS'
-
+c
 createServer(page =>
   createInertiaApp({
     page,
@@ -16,7 +15,6 @@ createServer(page =>
     setup({ app, props, plugin }) {
       return createSSRApp({ render: () => h(app, props) })
         .use(plugin)
-        // .use(HighchartsVue, { tagName: 'highcharts' })
         .use(ZiggyVue, {
           ...page.props.ziggy,
           location: new URL(page.props.ziggy.location),
