529b159697df623218c44178f45bc1df69284f13
phojie rengel <phojrengel@gmail.com>
2022-11-03 11:56:07 +0800

    refactor: use the route composable to determine if its active

--- resources/js/Components/Admin/AdminNavigation.vue
+++ resources/js/Components/Admin/AdminNavigation.vue
@@ -9,8 +9,8 @@ const { adminItems, miscItems } = useNavigation()
         <JLink
           v-for="item in adminItems" :key="item.name"
           :to="item.href"
-          :active="$page.url === item.href"
-          active-class="bg-primary-800 text-white"
+          :active="useRoute().isActive(item.href)"
+          active-class="text-white bg-primary-800"
           class="flex items-center px-2 py-2 text-sm font-medium leading-6 rounded-md group text-primary-100 hover:text-white hover:bg-primary-600"
         >
           <component :is="item.icon" size="24" class="flex-shrink-0 mr-4 text-primary-200" aria-hidden="true" />
@@ -23,8 +23,8 @@ const { adminItems, miscItems } = useNavigation()
           v-for="item in miscItems"
           :key="item.name"
           :to="item.href"
-          :active="$page.url === item.href"
-          active-class="bg-primary-800 text-white"
+          :active="useRoute().isActive(item.href)"
+          active-class="text-white bg-primary-800"
           class="flex items-center px-2 py-2 text-sm font-medium leading-6 rounded-md group text-primary-100 hover:text-white hover:bg-primary-600"
         >
           <component :is="item.icon" size="24" class="flex-shrink-0 mr-4 text-primary-200" aria-hidden="true" />
@@ -39,7 +39,7 @@ const { adminItems, miscItems } = useNavigation()
           All rights reserved
           © 2022-2023
         </div>
-        <div class="text-xs text-primary-300 flex space-x-2 items-center">
+        <div class="flex items-center space-x-2 text-xs text-primary-300">
           <span>
             App version 0.2.0
           </span>
