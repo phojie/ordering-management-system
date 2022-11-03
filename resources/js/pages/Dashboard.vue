441188a19288b2a0a08558e2749f8bc0bbb7c65b
phojie rengel <phojrengel@gmail.com>
2022-11-03 14:26:45 +0800

    style: set div wrapper to avoid hydration bug

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,17 +3,19 @@ const auth = useAuthStore()
 </script>
 
 <template>
-  <AppHeader />
+  <div>
+    <AppHeader />
 
-  <h2 class="text-xl font-semibold leading-tight text-gray-800">
-    Dashboard test
-  </h2>
+    <h2 class="text-xl font-semibold leading-tight text-gray-800">
+      Dashboard test
+    </h2>
 
-  <div class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200">
-          You're logged in! {{ auth.user.name }}
+    <div class="py-12">
+      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            You're logged in! {{ auth.user.name }}
+          </div>
         </div>
       </div>
     </div>
