289996741a2b5ad4193bd1f899731f4c04dc68bf
phojie rengel <phojrengel@gmail.com>
2022-11-07 12:00:07 +0800

    style: set indent to 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,72 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
-use App\Models\User;
-use Inertia\Controller;
-use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Controller;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $users = User::query()
-            ->where('id', '!=', auth()->user()->id)
-            ->orderBy('created_at', 'desc')
-            ->paginate(15);
+	public function index()
+	{
+		$users = User::query()
+			->where('id', '!=', auth()->user()->id)
+			->orderBy('created_at', 'desc')
+			->paginate(100);
 
-        $query = UserResource::collection($users);
+		$query = UserResource::collection($users);
 
-        return inertia('Admin/Users', [
-            'users' => $query
-        ]);
-    }
+		return inertia('Admin/Users', [
+		  'users' => $query,
+	  ]);
+	}
 
-    public function create()
-    {
-        //
-    }
+	public function create()
+	{
+		//
+	}
 
-    public function store(UserRequest $userRequest)
-    {
-        $userRequest->validated();
+	public function store(UserRequest $userRequest)
+	{
+		$userRequest->validated();
 
-        User::create([
-            'username' => $userRequest->username,
-            'email' => $userRequest->email,
-            'first_name' => $userRequest->firstName,
-            'last_name' => $userRequest->lastName,
-            'image_url' => $userRequest->imageUrl,
-            'password' => bcrypt($userRequest->password),
-        ]);
+		User::create([
+			'username' => $userRequest->username,
+			'email' => $userRequest->email,
+			'first_name' => $userRequest->firstName,
+			'last_name' => $userRequest->lastName,
+			'image_url' => $userRequest->imageUrl,
+			'password' => bcrypt($userRequest->password),
+		]);
 
-        return redirect()->back()->with('success', 'User created successfully.');
-    }
+		return redirect()->back()->with('success', 'User created successfully.');
+	}
 
-    public function show(User $user)
-    {
-        //
-    }
+	public function show(User $user)
+	{
+		//
+	}
 
-    public function edit(User $user)
-    {
-        //
-    }
+	public function edit(User $user)
+	{
+		//
+	}
 
-    public function update(UserRequest $userRequest, User $user)
-    {
-        //
-    }
+	public function update(UserRequest $userRequest, User $user)
+	{
+		//
+	}
 
-    public function destroy(User $user)
-    {
-        $user->delete();
-        return redirect()->back();
-    }
+	public function destroy(User $user)
+	{
+		$user->delete();
 
-    public function destroyMultiple(Request $request)
-    {
-        User::whereIn('id', $request->ids)->delete();
-        return redirect()->back();
-    }
+		return redirect()->back();
+	}
+
+	public function destroyMultiple(Request $request)
+	{
+		User::whereIn('id', $request->ids)->delete();
+
+		return redirect()->back();
+	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,53 +10,53 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
-    use HasFactory;
-    use Notifiable;
-    use HasUlids;
+	use HasApiTokens;
+	use HasFactory;
+	use Notifiable;
+	use HasUlids;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'username',
-        'email',
-        'first_name',
-        'middle_name',
-        'last_name',
-        'image_url',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'username',
+		'email',
+		'first_name',
+		'middle_name',
+		'last_name',
+		'image_url',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    protected static function boot()
-    {
-        parent::boot();
+	protected static function boot()
+	{
+		parent::boot();
 
-        static::creating(function ($model) {
-            if (! $model->image_url) {
-                $model->image_url = 'https://robohash.org/'.$model->id.'?set=set3&bgset=bg2&size=400x400';
-            }
-        });
-    }
+		static::creating(function ($model) {
+			if (! $model->image_url) {
+				$model->image_url = 'https://robohash.org/'.$model->id.'?set=set3&bgset=bg2&size=400x400';
+			}
+		});
+	}
 }
