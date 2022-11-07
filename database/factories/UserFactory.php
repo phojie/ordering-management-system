7e026b7d86ad48c7f1872b90f83fd85dbaa71e39
phojie rengel <phojrengel@gmail.com>
2022-11-07 12:03:39 +0800

    refactor: set function typings

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,35 +10,25 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'username' => fake()->username(),
-            'email' => fake()->unique()->safeEmail(),
-            'first_name' => fake()->firstName(),
-            'middle_name' => fake()->optional()->firstName(),
-            'last_name' => fake()->lastName(),
-            'image_url' =>'https://robohash.org/' . fake()->username() . '?set=set3&bgset=bg2&size=400x400',
-            'email_verified_at' => now(),
-            'password' => bcrypt('password') , // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+	public function definition() : array
+	{
+		return [
+			'username' => fake()->username(),
+			'email' => fake()->unique()->safeEmail(),
+			'first_name' => fake()->firstName(),
+			'middle_name' => fake()->optional()->firstName(),
+			'last_name' => fake()->lastName(),
+			'image_url' =>'https://robohash.org/'.fake()->username().'?set=set3&bgset=bg2&size=400x400',
+			'email_verified_at' => now(),
+			'password' => bcrypt('password'), // password
+			'remember_token' => Str::random(10),
+		];
+	}
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+	public function unverified() : static
+	{
+		return $this->state(fn (array $attributes) => [
+			'email_verified_at' => null,
+		]);
+	}
 }
