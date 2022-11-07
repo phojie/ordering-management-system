767598117b637dce43b6f3436ddb7f40b2910fc2
phojie rengel <phojrengel@gmail.com>
2022-11-07 14:51:58 +0800

    style: sync indentation

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,29 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+	return Inertia::render('Welcome', [
+		'canLogin' => Route::has('login'),
+		'canRegister' => Route::has('register'),
+		'laravelVersion' => Application::VERSION,
+		'phpVersion' => PHP_VERSION,
+	]);
 });
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+	Route::get('/dashboard', function () {
+		return Inertia::render('Dashboard');
+	})->name('dashboard');
 
-    Route::get('/admin', function () {
-        return Inertia::render('Admin/Index');
-    });
+	Route::get('/admin', function () {
+		return Inertia::render('Admin/Index');
+	});
 
-    Route::get('/admin/menus', function () {
-        return Inertia::render('Admin/Menus', [
-            'menus' => [],
-        ]);
-    });
+	Route::get('/admin/menus', function () {
+		return Inertia::render('Admin/Menus', [
+			'menus' => [],
+		]);
+	});
 });
 
-require __DIR__ . '/module/auth.php';
-require __DIR__ . '/module/user.php';
+require __DIR__.'/module/auth.php';
+require __DIR__.'/module/user.php';
