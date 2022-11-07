ea7b5b6524d88b4cd72fff50356b099ac0d8a91f
phojie rengel <phojrengel@gmail.com>
2022-11-07 09:51:58 +0800

    refactor: run php-cs-fixer

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,7 +10,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasUlids;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use HasUlids;
 
     /**
      * The attributes that are mass assignable.
