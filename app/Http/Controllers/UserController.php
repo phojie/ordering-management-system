47134dbdf0d237878f43fa23e73291bd758cfb68
phojie rengel <phojrengel@gmail.com>
2022-11-07 09:51:39 +0800

    refactor: use inertia global function instead

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,49 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Inertia\Inertia;
 use Inertia\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $users = User::query()
             ->where('id', '!=', auth()->user()->id)
             ->orderBy('created_at', 'desc')
             ->paginate(15);
-        $query =  UserResource::collection($users);
-        return Inertia::render('Admin/Users', [
+
+        $query = UserResource::collection($users);
+
+        return inertia('Admin/Users', [
             'users' => $query
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+    //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(UserRequest $userRequest)
     {
-       $userRequest->validated();
+        $userRequest->validated();
+
         User::create([
             'username' => $userRequest->username,
             'email' => $userRequest->email,
@@ -59,62 +45,30 @@ class UserController extends Controller
         return redirect()->back()->with('success', 'User created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function show(User $user)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function edit(User $user)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function update(UserRequest $userRequest, User $user)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
         $user->delete();
         return redirect()->back();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
     public function destroyMultiple(Request $request)
     {
-        User::whereIn('id',$request->ids)->delete();
+        User::whereIn('id', $request->ids)->delete();
         return redirect()->back();
     }
-
 }
