e80d141479c8ea5b0e68d0dff11d03384dba53c2
phojie rengel <phojrengel@gmail.com>
2022-11-07 12:31:35 +0800

    style: set types to function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        \App\Models\User::factory(10)->create();
-    }
+	public function run() : void
+	{
+		\App\Models\User::factory(100)->create();
+	}
 }
