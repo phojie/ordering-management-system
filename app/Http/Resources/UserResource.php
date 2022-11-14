902050313a81e4feec573d30f60381449cfc8e25
phojie rengel <phojrengel@gmail.com>
2022-11-14 18:13:22 +0800

    style: define types

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,26 +6,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-            'id' => $this->id,
-            'username' => $this->username,
-            'email' => $this->email,
-            'firstName' => $this->first_name,
-            'middleName' => $this->middle_name,
-            'lastName' => $this->last_name,
-            'fullName' => $this->full_name,
-            'imageUrl' => $this->image_url,
-            'emailVerifiedAt' => $this->email_verified_at,
-            'createdAt' => $this->created_at,
-            'status' => $this->status,
-        ];
-    }
+	public function toArray($request): array
+	{
+		return [
+			'id' => $this->id,
+			'username' => $this->username,
+			'email' => $this->email,
+			'firstName' => $this->first_name,
+			'middleName' => $this->middle_name,
+			'lastName' => $this->last_name,
+			'fullName' => $this->full_name,
+			'imageUrl' => $this->image_url,
+			'emailVerifiedAt' => $this->email_verified_at,
+			'createdAt' => $this->created_at,
+			'status' => $this->status,
+		];
+	}
 }
