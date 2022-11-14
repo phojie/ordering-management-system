d86c00f53397b3ad1b6de66ec6a0990db7612278
phojie rengel <phojrengel@gmail.com>
2022-11-14 17:00:43 +0800

    refactor: use name instead of username to users header

--- resources/js/composables/store/user.ts
+++ resources/js/composables/store/user.ts
@@ -36,32 +36,28 @@ export const useUserStore = defineStore('user', () => {
   })
 
   // table headers
-  const headers = ref([
+  const headers = reactive([
     {
-      text: 'Username',
-      value: 'username',
-      class: 'min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900',
+      text: 'Name',
+      value: 'full_name',
+      class: 'min-w-[12rem]',
+      sortable: true,
     },
     {
       text: 'Title',
       value: 'title',
-      class: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900',
     },
     {
       text: 'Role',
       value: 'role',
-      class: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900',
     },
     {
       text: 'Status',
       value: 'status',
-      class: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900',
     },
     {
       text: 'Actions',
       value: 'actions',
-      class: 'px-3 py-3.5 text-left text-sm font-semibold text-gray-900',
-      sortable: false,
     },
   ])
 
