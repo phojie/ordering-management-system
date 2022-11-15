6e92bc0d7f15da7fc914f0450f77ea4ad278214d
phojie rengel <phojrengel@gmail.com>
2022-11-16 04:19:01 +0800

    style: use icons instead of text in actions column

--- resources/js/components/Admin/Users/AdminUsersTable.vue
+++ resources/js/components/Admin/Users/AdminUsersTable.vue
@@ -83,19 +83,22 @@ const toggleEdit = (user: User) => {
             v-if="item.status === 'active'" :disabled="processing" type="button"
             class="text-primary-600 hover:text-primary-900" @click="toggleEdit(item as any)"
           >
-            <!-- <heroicons-pencil-square-20-solid class="w-5 h-5" /> -->
-            Edit
+            <heroicons-pencil-square-20-solid class="w-5 h-5" />
+            <!-- Edit -->
             <span class="sr-only">, {{ item.name }}</span>
           </button>
 
           <button
-            v-if="item.status === 'deleted'" :disabled="processing" type="button"
+            v-if="item.status === 'deleted'"
+            :disabled="processing"
+            type="button"
             class="text-warning-600 hover:text-warning-900" @click="restoreUser(item.id)"
           >
-            Restore
-            <!-- <heroicons-arrow-path-rounded-square-20-solid class="w-5 h-5" /> -->
+            <!-- Restore -->
+            <heroicons-arrow-path-rounded-square-20-solid class="w-5 h-5" />
             <span class="sr-only">, {{ item.name }}</span>
           </button>
+
           <div v-if="false" class="flex items-center space-x-2 ">
             <button v-if="item.status === 'active'" :disabled="processing" type="button" class="text-danger-600 hover:text-danger-900" @click="deleteUser(item.id)">
               <heroicons-trash-20-solid class="w-5 h-5" />
