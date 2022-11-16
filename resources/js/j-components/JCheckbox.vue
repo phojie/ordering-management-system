66ffef3c956e260ef1927d24ba01d77f25fb722c
phojie rengel <phojrengel@gmail.com>
2022-11-17 02:37:04 +0800

    refactor(j-components): use VModel binding instead of manually emit

--- resources/js/j-components/JCheckbox.vue
+++ resources/js/j-components/JCheckbox.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
 import type { Checkbox } from './types'
 
-withDefaults(defineProps<Checkbox>(), {
+const props = withDefaults(defineProps<Checkbox>(), {
   modelValue: false,
 })
 
 // set emits
-defineEmits<{
-  (e: 'update:modelValue', value: boolean): void
-}>()
+const emit = defineEmits(['update:modelValue'])
+const value = useVModel(props, 'modelValue', emit)
 </script>
 
 <template>
   <div class="flex items-center">
     <input
       :id="id"
+      v-model="value"
       :readonly="isReadOnly"
       :disabled="isDisabled"
       :name="name"
       class="w-4 h-4 border-gray-300 rounded text-primary-600 focus:ring-primary-500"
       type="checkbox"
-      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).checked)"
     >
     <label :for="id" class="block ml-2 text-sm text-gray-900">
       {{ label }}
