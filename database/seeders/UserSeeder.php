1ef8770952239bf7b143b48fd3fa17565ce948e9
phojie rengel <phojrengel@gmail.com>
2022-11-18 02:41:26 +0800

    feat: set new user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+	public function run(): void
+	{
+		User::factory(100)->create();
+	}
+}
