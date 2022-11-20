e6628bffd56ab5025120da0b9641802cdb386134
phojie rengel <phojrengel@gmail.com>
2022-11-21 07:28:10 +0800

    fix: remove role relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -39,11 +38,6 @@ class User extends Authenticatable
 	protected $casts = [
 		'email_verified_at' => 'datetime',
 	];
-
-	public function roles(): BelongsToMany
-	{
-		return $this->belongsToMany(Role::class);
-	}
 
 	public function scopeSearch($query, $search): object
 	{
