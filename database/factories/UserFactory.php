f8e72958f13a47e7d4220cdcd8d0344d3bce6363
phojie rengel <phojrengel@gmail.com>
2022-11-21 07:27:05 +0800

    chore: set default role to user factory automatically

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -28,4 +29,11 @@ class UserFactory extends Factory
 			'email_verified_at' => null,
 		]);
 	}
+
+  public function configure(): static
+  {
+  	return $this->afterCreating(function (User $user) {
+  		$user->assignRole('customer');
+  	});
+  }
 }
