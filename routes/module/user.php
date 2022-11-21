6a40a0d281ef05c41d2ff9d1d2ece4024167bb5e
phojie rengel <phojrengel@gmail.com>
2022-11-22 07:40:22 +0800

    refactor: set prefix user route name

--- routes/module/user.php
+++ routes/module/user.php
@@ -3,25 +3,25 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->middleware('auth')->group(function () {
+Route::prefix('admin/users')->middleware('auth')->group(function () {
 	Route::controller(UserController::class)->group(function () {
     // delete
-		Route::delete('/users/{user}', 'destroy')->name('users.destroy');
-		Route::delete('/users', 'destroyMultiple')->name('users.destroy-multiple');
+		Route::delete('/{user}', 'destroy')->name('users.destroy');
+		Route::delete('/', 'destroyMultiple')->name('users.destroy-multiple');
 
     // restore
-		Route::put('/users/{user}/restore', 'restore')->name('users.restore')->withTrashed();
-		Route::put('/users/restore', 'restoreMultiple')->name('users.restore-multiple');
+		Route::put('/{user}/restore', 'restore')->name('users.restore')->withTrashed();
+		Route::put('/restore', 'restoreMultiple')->name('users.restore-multiple');
 
     // index
-		Route::get('/users', 'index')->name('users.index');
+		Route::get('/', 'index')->name('users.index');
 
     // create
-		Route::post('/users', 'store')->name('users.store');
+		Route::post('/', 'store')->name('users.store');
 
     // Route::get('/users/{user}', 'show')->name('users.show');
 
     // update
-		Route::put('/users/{user}', 'update')->name('users.update');
+		Route::put('/{user}', 'update')->name('users.update');
 	});
 });
