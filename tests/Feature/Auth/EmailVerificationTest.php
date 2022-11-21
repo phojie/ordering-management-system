370dd945c72e7c0fc03955b1cd9638effd92179e
phojie rengel <phojrengel@gmail.com>
2022-11-21 19:58:21 +0800

    style: format lines

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -12,54 +12,54 @@ use Tests\TestCase;
 
 class EmailVerificationTest extends TestCase
 {
-    use RefreshDatabase;
+	use RefreshDatabase;
 
-    public function test_email_verification_screen_can_be_rendered()
-    {
-        $user = User::factory()->create([
-            'email_verified_at' => null,
-        ]);
+	public function test_email_verification_screen_can_be_rendered()
+	{
+		$user = User::factory()->create([
+			'email_verified_at' => null,
+		]);
 
-        $response = $this->actingAs($user)->get('/verify-email');
+		$response = $this->actingAs($user)->get('/verify-email');
 
-        $response->assertStatus(200);
-    }
+		$response->assertStatus(200);
+	}
 
-    public function test_email_can_be_verified()
-    {
-        $user = User::factory()->create([
-            'email_verified_at' => null,
-        ]);
+	public function test_email_can_be_verified()
+	{
+		$user = User::factory()->create([
+			'email_verified_at' => null,
+		]);
 
-        Event::fake();
+		Event::fake();
 
-        $verificationUrl = URL::temporarySignedRoute(
-            'verification.verify',
-            now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1($user->email)]
-        );
+		$verificationUrl = URL::temporarySignedRoute(
+			'verification.verify',
+			now()->addMinutes(60),
+			['id' => $user->id, 'hash' => sha1($user->email)]
+		);
 
-        $response = $this->actingAs($user)->get($verificationUrl);
+		$response = $this->actingAs($user)->get($verificationUrl);
 
-        Event::assertDispatched(Verified::class);
-        $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
-    }
+		Event::assertDispatched(Verified::class);
+		$this->assertTrue($user->fresh()->hasVerifiedEmail());
+		$response->assertRedirect(RouteServiceProvider::HOME.'?verified=1');
+	}
 
-    public function test_email_is_not_verified_with_invalid_hash()
-    {
-        $user = User::factory()->create([
-            'email_verified_at' => null,
-        ]);
+	public function test_email_is_not_verified_with_invalid_hash()
+	{
+		$user = User::factory()->create([
+			'email_verified_at' => null,
+		]);
 
-        $verificationUrl = URL::temporarySignedRoute(
-            'verification.verify',
-            now()->addMinutes(60),
-            ['id' => $user->id, 'hash' => sha1('wrong-email')]
-        );
+		$verificationUrl = URL::temporarySignedRoute(
+			'verification.verify',
+			now()->addMinutes(60),
+			['id' => $user->id, 'hash' => sha1('wrong-email')]
+		);
 
-        $this->actingAs($user)->get($verificationUrl);
+		$this->actingAs($user)->get($verificationUrl);
 
-        $this->assertFalse($user->fresh()->hasVerifiedEmail());
-    }
+		$this->assertFalse($user->fresh()->hasVerifiedEmail());
+	}
 }
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -8,30 +8,30 @@ use Tests\TestCase;
 
 class RegistrationTest extends TestCase
 {
-    use RefreshDatabase;
+	use RefreshDatabase;
 
-    public function test_registration_screen_can_be_rendered()
-    {
-        $response = $this->get('/register');
+	public function test_registration_screen_can_be_rendered()
+	{
+		$response = $this->get('/register');
 
-        $response->assertStatus(200);
-    }
+		$response->assertStatus(200);
+	}
 
-    public function test_new_users_can_register()
-    {
-        $response = $this->post('/register', [
-            'username' => 'Test User',
-            'email' => 'test@example.com',
-            'firstName' => 'Test',
-            'middleName' => 'User',
-            'imageUrl' => 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-            'lastName' => 'Test',
+	public function test_new_users_can_register()
+	{
+		$response = $this->post('/register', [
+			'username' => 'Test User',
+			'email' => 'test@example.com',
+			'firstName' => 'Test',
+			'middleName' => 'User',
+			'imageUrl' => 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+			'lastName' => 'Test',
 
-            'password' => 'password',
-            'passwordConfirmation' => 'password',
-        ]);
+			'password' => 'password',
+			'passwordConfirmation' => 'password',
+		]);
 
-        $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
-    }
+		$this->assertAuthenticated();
+		$response->assertRedirect(RouteServiceProvider::HOME);
+	}
 }
