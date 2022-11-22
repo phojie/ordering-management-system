e324cfff35f5e14f9f2f2f839ff9f14d5d8d1dc3
phojie rengel <phojrengel@gmail.com>
2022-11-23 07:31:23 +0800

    style: format annotation spacing

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -6,24 +6,23 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
 {
-	public function authorize() : bool
+	public function authorize(): bool
 	{
 		return true;
 	}
 
-   public function rules() : array
+   public function rules(): array
    {
    	$rules = [
-      'username' => ['required', 'unique:users'],
-      // 'roles' => ['required', 'array', 'min:1'],
-      'password' => ['required', 'min:6'],
-      'firstName' => ['required'],
-      'lastName' => ['required'],
-      'email' => ['required', 'email', 'unique:users'],
-    ];
+   		'username' => ['required', 'unique:users'],
+   		// 'roles' => ['required', 'array', 'min:1'],
+   		'password' => ['required', 'min:6'],
+   		'firstName' => ['required'],
+   		'lastName' => ['required'],
+   		'email' => ['required', 'email', 'unique:users'],
+   	];
 
    	if ($this->getMethod() == 'PUT') {
-   		// unique except present present username, detect by id
    		$rules['username'][1] = 'unique:users,username,'.request()->id;
    		$rules['password'] = [];
    		$rules['email'] = 'unique:users,email,'.request()->id;
