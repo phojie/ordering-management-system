6ee80ac443adf0d45ed9b4b9d42e963a2047279a
phojie rengel <phojrengel@gmail.com>
2022-11-23 07:32:16 +0800

    style: format lines

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,25 +7,15 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-         if ($this->app->environment('local')) {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
-    }
+	public function register(): void
+	{
+		if ($this->app->environment('local')) {
+			$this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+		}
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        JsonResource::withoutWrapping();
-    }
+	public function boot(): void
+	{
+		JsonResource::withoutWrapping();
+	}
 }
