d0d49ab263133f79b882cd823a97ac11cc4b523a
phojie rengel <phojrengel@gmail.com>
2022-11-27 15:02:53 +0800

    refactor: set currentUrl as global variable

--- resources/js/composables/app/route.ts
+++ resources/js/composables/app/route.ts
@@ -1,13 +1,14 @@
 export const useRoute = () => {
+  const currentRoute = route().current() as string
+  const currentUrl = usePage().url.value
+
   function isActive(href: string, exact = false) {
-    const currentUrl = usePage().url.value
+    // const currentUrl = usePage().url.value
     if (exact)
-      return currentUrl.startsWith(href)
+      return _.startsWith(currentUrl, href)
 
     return _.trim(href, '/') === _.trim(currentUrl, '/')
   }
-
-  const currentRoute = route().current() as string
 
   const defaultSearch = _.get(route().params, 'search', '') as string
   const defaultRows = _.get(route().params, 'rows', '10') as string
@@ -17,6 +18,7 @@ export const useRoute = () => {
     defaultSearch,
 
     currentRoute,
+    currentUrl,
   }
 }
 
