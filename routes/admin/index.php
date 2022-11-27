08c82c8170a89b1cf7ae2f34b1178eddc784c5e5
phojie rengel <phojrengel@gmail.com>
2022-11-27 16:55:17 +0800

    chore: config settings routes

--- routes/admin/index.php
+++ routes/admin/index.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\SettingController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware('auth')->group(function () {
-  Route::get('/settings', [SettingController::class, 'index'])
-    ->name('settings.index');
+	Route::get('/settings', [SettingController::class, 'index'])->name('settings.index');
+
+	Route::put('/settings', [SettingController::class, 'update'])->name('settings.update');
 });
