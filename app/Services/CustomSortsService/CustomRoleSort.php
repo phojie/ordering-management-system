be8b1f9945d22985fab4b5895c20334315dd6d42
phojie rengel <phojrengel@gmail.com>
2022-12-03 17:50:56 +0800

    style: set antonation

--- app/Services/CustomSortsService/CustomRoleSort.php
+++ app/Services/CustomSortsService/CustomRoleSort.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 class CustomRoleSort implements \Spatie\QueryBuilder\Sorts\Sort
 {
-	public function __invoke(Builder $query, bool $descending, string $property)
+	public function __invoke(Builder $query, bool $descending, string $property): void
 	{
 		$direction = $descending ? 'DESC' : 'ASC';
 
