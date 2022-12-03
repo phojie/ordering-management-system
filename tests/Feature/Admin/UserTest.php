276fb6d904de5471b522b1bb35c02cc07ae518fa
phojie rengel <phojrengel@gmail.com>
2022-12-04 00:13:47 +0800

    test: set setUp for user test

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -10,106 +10,106 @@ class UserTest extends TestCase
 {
 	use RefreshDatabase;
 
+	public $response;
+
+	public $user;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->seed();
+
+		$this->user = User::where('email', 'j@y.com')->first();
+
+		$this->response = $this->actingAs($this->user);
+	}
+
 	public function test_admin_users_can_render_page()
 	{
-		$user = User::factory()->create();
+		$this->response = $this->get('/admin/users');
 
-		$response = $this->actingAs($user);
+		$this->response->assertStatus(200);
+	}
 
-		$response = $this->get('/admin/users');
+	public function test_admin_users_can_store_user()
+	{
+		$response = $this->response;
+
+		// post user
+		$response = $this->post('/admin/users', [
+			'username' => 'test',
+			'email' => 'test@y.com',
+			'firstName' => 'test',
+			'lastName' => 'test',
+			'password' => 'password',
+		]);
+
+		\Log::info($response->getContent());
 
 		$response->assertStatus(200);
 	}
 
-  public function test_admin_users_can_create_user()
-  {
-  	$user = User::factory()->create();
+	public function test_admin_users_can_update_user()
+	{
+		$user = $this->user;
 
-  	$response = $this->actingAs($user);
+		// post user
+		$response = $this->put("/admin/users/$user->id", [
+			'username' => 'test2',
+			'email' => 'test@y.com',
+			'firstName' => 'test',
+			'lastName' => 'test',
+			'password' => 'password',
+		]);
 
-  	// post user
-  	$response = $this->post('/admin/users', [
-  		'username' => 'test',
-  		'email' => 'test@y.com',
-  		'firstName' => 'test',
-  		'lastName' => 'test',
-  		'password' => 'password',
-  	]);
+		$response->assertStatus(302);
+	}
 
-  	$response->assertStatus(302);
-  }
+	public function test_admin_users_can_delete_user()
+	{
+		$user = $this->user;
+		// post user
+		$response = $this->delete("/admin/users/$user->id");
 
-  public function test_admin_users_can_update_user()
-  {
-  	$user = User::factory()->create();
+		$response->assertStatus(302);
+	}
 
-  	$response = $this->actingAs($user);
+	public function test_admin_users_can_restore_user()
+	{
+		$user = $this->user;
+		// post user
+		$response = $this->put("/admin/users/$user->id/restore");
 
-  	// post user
-  	$response = $this->put("/admin/users/$user->id", [
-  		'username' => 'test2',
-  		'email' => 'test@y.com',
-  		'firstName' => 'test',
-  		'lastName' => 'test',
-  		'password' => 'password',
-  	]);
+		$response->assertStatus(302);
+	}
 
-  	$response->assertStatus(302);
-  }
+	public function test_admin_users_can_delete_multiple_users()
+	{
+		$user = $this->user;
+		$users = User::factory()->count(3)->create()->pluck('id')->toArray();
 
-  public function test_admin_users_can_delete_user()
-  {
-  	$user = User::factory()->create();
+		$response = $this->actingAs($user);
 
-  	$response = $this->actingAs($user);
+		$response = $this->delete('/admin/users', [
+			'ids' => $users,
+		]);
 
-  	// post user
-  	$response = $this->delete("/admin/users/$user->id");
+		$response->assertStatus(302);
+	}
 
-  	$response->assertStatus(302);
-  }
+	public function test_admin_users_can_restore_multiple_users()
+	{
+		$users = User::factory()->count(3)->create()->pluck('id')->toArray();
 
-  public function test_admin_users_can_restore_user()
-  {
-  	$user = User::factory()->create();
+		$response = $this->delete('/admin/users', [
+			'ids' => $users,
+		]);
 
-  	$response = $this->actingAs($user);
+		$response = $this->put('/admin/users/restore', [
+			'ids' => $users,
+		]);
 
-  	// post user
-  	$response = $this->put("/admin/users/$user->id/restore");
-
-  	$response->assertStatus(302);
-  }
-
-  public function test_admin_users_can_delete_multiple_users()
-  {
-  	$user = User::factory()->create();
-  	$users = User::factory()->count(3)->create()->pluck('id')->toArray();
-
-  	$response = $this->actingAs($user);
-
-  	$response = $this->delete('/admin/users', [
-  		'ids' => $users,
-  	]);
-
-  	$response->assertStatus(302);
-  }
-
-  public function test_admin_users_can_restore_multiple_users()
-  {
-  	$user = User::factory()->create();
-  	$users = User::factory()->count(3)->create()->pluck('id')->toArray();
-
-  	$response = $this->actingAs($user);
-
-  	$response = $this->delete('/admin/users', [
-  		'ids' => $users,
-  	]);
-
-  	$response = $this->put('/admin/users/restore', [
-  		'ids' => $users,
-  	]);
-
-  	$response->assertStatus(302);
-  }
+		$response->assertStatus(302);
+	}
 }
