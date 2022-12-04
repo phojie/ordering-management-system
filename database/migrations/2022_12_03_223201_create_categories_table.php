28f70cfcb38620270b74218183e8022067d6e415
phojie rengel <phojrengel@gmail.com>
2022-12-04 17:48:44 +0800

    feat: add category database

---
+++ database/migrations/2022_12_03_223201_create_categories_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->string('slug')->unique();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+};
