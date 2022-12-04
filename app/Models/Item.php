0b9b25708c2ca557eb0dc48dc7535bd9c59c6760
phojie rengel <phojrengel@gmail.com>
2022-12-04 23:40:30 +0800

    feat: add item model

---
+++ app/Models/Item.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use HasFactory;
+    use HasUuids;
+    use SoftDeletes;
+
+    public $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+    public function scopeSearch($query, $search): object
+    {
+      return $query->when($search, fn ($q) => $q->where('name', 'ilike', "%{$search}%")
+        ->orWhere('description', 'ilike', "%{$search}%"));
+    }
+}
