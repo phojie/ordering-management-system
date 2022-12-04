93c4563914da03d1c65d2cf49473ca3a60374e34
phojie rengel <phojrengel@gmail.com>
2022-12-05 00:06:20 +0800

    style: format item model file

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -4,25 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    use HasFactory;
-    use HasUuids;
-    use SoftDeletes;
+	use HasFactory;
+	use HasUuids;
+	use SoftDeletes;
 
-    public $fillable = [
-        'name',
-        'slug',
-        'description',
-    ];
+	public $fillable = [
+		'name',
+		'description',
+	];
 
-    public function scopeSearch($query, $search): object
-    {
-      return $query->when($search, fn ($q) => $q->where('name', 'ilike', "%{$search}%")
-        ->orWhere('description', 'ilike', "%{$search}%"));
-    }
+	public function scopeSearch($query, $search): object
+	{
+		return $query->when($search, fn ($q) => $q->where('name', 'ilike', "%{$search}%")
+		->orWhere('description', 'ilike', "%{$search}%"));
+	}
 }
