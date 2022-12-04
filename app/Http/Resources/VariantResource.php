744da7a88aa851a78334da7c67a8a628890c9529
phojie rengel <phojrengel@gmail.com>
2022-12-05 02:10:11 +0800

    feat: add image to items variant

--- app/Http/Resources/VariantResource.php
+++ app/Http/Resources/VariantResource.php
@@ -14,6 +14,7 @@ class VariantResource extends JsonResource
 			'status' => $this->status,
 			'stock' => $this->stock,
 			'price' => $this->price,
+      'image' => $this->image,
       'color' => $this->color,
 			'createdAt' => $this->created_at,
 			'updatedAt' => $this->updated_at,
--- app/Models/Variant.php
+++ app/Models/Variant.php
@@ -5,27 +5,52 @@ namespace App\Models;
 use App\Services\MiscServices;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Variant extends Model
+class Variant extends Model implements HasMedia
 {
-    use HasFactory;
-    use HasUuids;
+	use HasFactory;
+	use HasUuids;
+	use InteractsWithMedia;
 
-    public $fillable = [
-      'name',
-      'status',
-      'stock',
-      'price',
-      'item_id',
-    ];
+	public $fillable = [
+		'name',
+		'status',
+		'stock',
+		'price',
+		'item_id',
+	];
 
-    public static function boot(): void
-    {
-      parent::boot();
-      self::creating(function ($model) {
-        $model->color = (new MiscServices())->getRandomColor();
-      });
-    }
+	protected $appends = [
+		'image',
+	];
+
+	public function getImageAttribute(): string
+	{
+		return $this->getFirstMediaUrl('image', 'thumb') ?: 'https://robohash.org/'.$this->id.'?set=set1&bgset=bg2&size=400x400';
+	}
+
+  public function registerMediaCollections(): void
+  {
+  	$this->addMediaCollection('image')
+  		  ->singleFile();
+  }
+
+  public function registerMediaConversions(Media $media = null): void
+  {
+  	$this->addMediaConversion('image')
+  		  ->width(400)
+  		  ->height(400);
+  }
+
+	public static function boot(): void
+	{
+		parent::boot();
+		self::creating(function ($model) {
+			$model->color = (new MiscServices())->getRandomColor();
+		});
+	}
 }
