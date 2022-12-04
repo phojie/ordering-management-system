6e8d28e04af302c7edad0d6b541120a84885d553
phojie rengel <phojrengel@gmail.com>
2022-12-05 00:57:45 +0800

    styke: set variant to item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
@@ -17,6 +18,11 @@ class Item extends Model
 		'name',
 		'description',
 	];
+
+  public function variants(): HasMany
+  {
+    return $this->hasMany(Variant::class);
+  }
 
 	public function scopeSearch($query, $search): object
 	{
