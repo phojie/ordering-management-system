7a2d64ddaa08c50fc5c0790daff3c1c62ed18e4e
phojie rengel <phojrengel@gmail.com>
2022-12-05 00:59:16 +0800

    feat: add variant factory

---
+++ database/factories/VariantFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use App\Models\Variant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Variant>
+ */
+class VariantFactory extends Factory
+{
+	protected $model = Variant::class;
+
+	public function definition()
+	{
+		return [
+			'name' => $this->faker->word(),
+			'status' => $this->faker->randomElement(['active', 'inactive']),
+			'stock' => $this->faker->randomNumber(),
+			'price' => $this->faker->randomFloat(2, 0, 100),
+			'item_id' => Item::inRandomOrder()->first()->id,
+		];
+	}
+}
