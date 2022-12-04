a6906366ba81f809a02476dc3050d72601f5af78
phojie rengel <phojrengel@gmail.com>
2022-12-04 23:40:49 +0800

    feat: add item database

---
+++ database/migrations/2022_12_04_152317_create_items_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable();
+            $table->string('status')->default('active');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+};
