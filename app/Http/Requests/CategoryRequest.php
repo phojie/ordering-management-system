36f570f7e9af4b23fa8d611cab12c49e5a98091b
phojie rengel <phojrengel@gmail.com>
2022-12-04 17:47:47 +0800

    style: set category request

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+	public function rules(): array
+	{
+		$rules = [
+			'name' => ['required', 'unique:categories'],
+			'description' => ['required', 'string', 'max:100'],
+		];
+
+		if ($this->getMethod() == 'PUT') {
+			$rules['name'][1] = 'unique:categories,name,'.request()->id;
+		}
+
+		return $rules;
+	}
+}
