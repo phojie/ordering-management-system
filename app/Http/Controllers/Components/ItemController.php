db6047245354fb15ff3fa7ef0e0b888432c90b35
phojie rengel <phojrengel@gmail.com>
2022-12-05 00:05:36 +0800

    feat: add item form

---
+++ app/Http/Controllers/Components/ItemController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Components;
+
+use App\Http\Resources\ItemResource;
+use App\Models\Item;
+
+class ItemController
+{
+	public function show($id)
+	{
+		$query = Item::query()
+				->find($id);
+		$item = new ItemResource($query);
+
+		return response()->json($item, 200);
+	}
+}
---
+++ resources/js/components/Form/FormItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { form, $v } = useItemStore()
+const processing = $computed(() => useItemStore().processing)
+</script>
+
+<template>
+  <div class="pt-6 pb-5 space-y-6">
+    <JTextField
+      id="name"
+      v-model="form.name"
+      placeholder="Name"
+      label="Name"
+      :is-disabled="processing"
+      :error-message="$v.name.$errors[0]?.$message"
+      :is-error="$v.name.$error"
+    />
+
+    <JTextArea
+      id="description"
+      v-model="form.description"
+      placeholder="Short description"
+      label="Description"
+      :is-disabled="processing"
+      :error-message="$v.description.$errors[0]?.$message"
+      :is-error="$v.description.$error"
+    />
+  </div>
+</template>
