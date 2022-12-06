b4f9eb3a694e52896453d96dcfb973ee50ec113e
phojie rengel <phojrengel@gmail.com>
2022-12-06 16:22:04 +0800

    chore: ide-helper imports config

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -241,8 +241,6 @@ namespace App\Models{
  * @property string $item_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection|\Spatie\MediaLibrary\MediaCollections\Models\Media[] $media
- * @property-read int|null $media_count
  * @method static \Database\Factories\VariantFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Variant newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Variant newQuery()
@@ -257,6 +255,6 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|Variant whereStock($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Variant whereUpdatedAt($value)
  */
-	class Variant extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+	class Variant extends \Eloquent {}
 }
 
