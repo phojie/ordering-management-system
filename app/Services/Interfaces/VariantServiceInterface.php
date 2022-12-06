39a08df1c458ed718693a34884af36b31cae1ff4
phojie rengel <phojrengel@gmail.com>
2022-12-07 00:58:30 +0800

    fix: sync variants to item when updated

--- app/Services/Interfaces/VariantServiceInterface.php
+++ app/Services/Interfaces/VariantServiceInterface.php
@@ -7,4 +7,6 @@ use App\Models\Item;
 interface VariantServiceInterface
 {
 	public function storeMultiple(object $request, Item $item): void;
+
+  public function updateMultiple(object $request, Item $item): void;
 }
--- app/Services/VariantService.php
+++ app/Services/VariantService.php
@@ -22,4 +22,27 @@ class VariantService implements VariantServiceInterface
 			(new FlashNotification())->error($e->getMessage());
 		}
 	}
+
+  public function updateMultiple(object $request, Item $item): void
+  {
+  	try {
+  		$variants = collect($request->variants)
+  			  ->map(function ($variant) use ($item) {
+  			  	return [
+  			  		'id' => \Str::of($variant['id'])->isUuid() ? $variant['id'] : \Str::uuid(),
+  			  		'item_id' => $item->id,
+  			  		'name' => $variant['name'],
+  			  		'price' => $variant['price'],
+  			  		'stock' => $variant['stock'],
+  			  	];
+  			  })
+  			  ->toArray();
+
+  		$item->variants()->upsert($variants, ['id'], ['name', 'price', 'stock']);
+
+  		$item->variants()->whereNotIn('id', collect($variants)->pluck('id'))->delete();
+  	} catch (\Exception $e) {
+  		abort(500, $e->getMessage());
+  	}
+  }
 }
