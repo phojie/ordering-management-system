a7c9b401bd46f8417f6b5640384104c9208785bc
phojie rengel <phojrengel@gmail.com>
2022-12-06 16:39:49 +0800

    style: format lines

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+	$this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
