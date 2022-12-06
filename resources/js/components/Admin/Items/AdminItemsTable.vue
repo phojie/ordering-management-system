03274ba5f3dd1177f1e99cf5f67c33810423be19
phojie rengel <phojrengel@gmail.com>
2022-12-07 01:43:08 +0800

    style: set form status when form update is trigger

--- resources/js/components/Admin/Items/AdminItemsTable.vue
+++ resources/js/components/Admin/Items/AdminItemsTable.vue
@@ -16,6 +16,7 @@ const deleteAll = () => {
 const toggleEdit = (item: Item) => {
   form.id = item.id
   form.name = item.name
+  form.status = item.status
   form.description = item.description
   form.variants = item.variants
 
@@ -120,23 +121,22 @@ const getById = async (id: number) => {
 
         <td class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6">
           <button
-            v-if="item.status === 'active'"
-            v-tooltip="'Edit item'"
-            type="button"
-            class="text-primary-600 hover:text-primary-900"
-            @click="getById(item.id)"
-          >
-            <heroicons-pencil-square-20-solid class="w-5 h-5" />
-          </button>
-
-          <button
-            v-else-if="item.status === 'deleted'"
+            v-if="item.status === 'deleted'"
             v-tooltip="'Restore item'"
             type="button"
             class="text-warning-600 hover:text-warning-900"
             @click="restoreItem(item.id)"
           >
             <heroicons-arrow-path-rounded-square-20-solid class="w-5 h-5" />
+          </button>
+          <button
+            v-else
+            v-tooltip="'Edit item'"
+            type="button"
+            class="text-primary-600 hover:text-primary-900"
+            @click="getById(item.id)"
+          >
+            <heroicons-pencil-square-20-solid class="w-5 h-5" />
           </button>
         </td>
       </template>
