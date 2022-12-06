fb47b0a6c770cbd33aaeda7a5a888577bff94dd0
phojie rengel <phojrengel@gmail.com>
2022-12-07 02:20:42 +0800

    style: format lines

--- database/migrations/2022_12_04_152317_create_items_table.php
+++ database/migrations/2022_12_04_152317_create_items_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('items', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+return new class extends Migration {
+	public function up(): void
+	{
+		Schema::create('items', function (Blueprint $table) {
+			$table->uuid('id')->primary();
 
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('description')->nullable();
-            $table->string('status')->default('active');
+			$table->string('name');
+			$table->string('slug')->unique();
+			$table->string('description')->nullable();
+			$table->string('status')->default('active');
 
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 };
