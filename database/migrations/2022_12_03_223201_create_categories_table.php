fcfd8db07ef97f3b25910a1fd59448927ede6f39
phojie rengel <phojrengel@gmail.com>
2022-12-07 04:20:16 +0800

    feat: create category-item database

--- database/migrations/2022_12_03_223201_create_categories_table.php
+++ database/migrations/2022_12_03_223201_create_categories_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+return new class extends Migration {
+	public function up(): void
+	{
+		Schema::create('categories', function (Blueprint $table) {
+			$table->uuid('id')->primary();
 
-            $table->string('name')->unique();
-            $table->string('color')->nullable();
-            $table->string('description')->nullable();
-            $table->string('slug')->unique();
+			$table->string('name')->unique();
+			$table->string('color')->nullable();
+			$table->string('description')->nullable();
+			$table->string('slug')->unique();
 
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
 };
