e2a9ceadcf294e6415802da03e961fa880525a6d
phojie rengel <phojrengel@gmail.com>
2022-12-30 06:25:01 +0800

    feat: set light and dark logo to app icon

--- resources/js/Components/App/AppIcon.vue
+++ resources/js/Components/App/AppIcon.vue
@@ -1,6 +1,23 @@
+<script setup lang="ts">
+interface AppIconProps {
+  dark?: boolean
+}
+
+const props = withDefaults(defineProps<AppIconProps>(), {
+  dark: false,
+})
+
+const logo = computed(() => {
+  if (props.dark)
+    return '/logo/dark.png'
+
+  return '/logo/light.png'
+})
+</script>
+
 <template>
   <!-- shade 600 -->
   <!-- <img src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600" alt="."> -->
-  <img src="/logo/light.png" alt="." class="rounded-full">
+  <img :src="logo" alt="." class="rounded-full">
   <!-- </div> -->
 </template>
