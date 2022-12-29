fa3e2bf3f7118b193650f358ab0c1b771f933a38
phojie rengel <phojrengel@gmail.com>
2022-12-30 06:14:21 +0800

    feat: add favicon and icons folder

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -12,48 +12,48 @@ use Inertia\Inertia;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     *
-     * @return \Inertia\Response
-     */
-    public function create()
-    {
-        return Inertia::render('Auth/Login', [
-            'canResetPassword' => Route::has('password.request'),
-            'status' => session('status'),
-        ]);
-    }
+	/**
+	 * Display the login view.
+	 *
+	 * @return \Inertia\Response
+	 */
+	public function create()
+	{
+		return Inertia::render('Auth/Login', [
+			'canResetPassword' => Route::has('password.request'),
+			'status' => session('status'),
+		]);
+	}
 
-    /**
-     * Handle an incoming authentication request.
-     *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(LoginRequest $request)
-    {
-        $request->authenticate();
+	/**
+	 * Handle an incoming authentication request.
+	 *
+	 * @param  \App\Http\Requests\Auth\LoginRequest  $request
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function store(LoginRequest $request)
+	{
+		$request->authenticate();
 
-        $request->session()->regenerate();
+		$request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
+		return redirect()->intended(RouteServiceProvider::HOME);
+	}
 
-    /**
-     * Destroy an authenticated session.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(Request $request)
-    {
-        Auth::guard('web')->logout();
+	/**
+	 * Destroy an authenticated session.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\RedirectResponse
+	 */
+	public function destroy(Request $request)
+	{
+		Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+		$request->session()->invalidate();
 
-        $request->session()->regenerateToken();
+		$request->session()->regenerateToken();
 
-        return redirect(route('index'));
-    }
+		return redirect(route('index'));
+	}
 }
