714b3188ddbcaf6e350157d316aed223cf330b6a
phojie rengel <phojrengel@gmail.com>
2022-12-30 22:51:35 +0800

    feat: add public routes folder

---
+++ routes/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+	return Inertia::render('Welcome', [
+		'canLogin' => Route::has('login'),
+		'canRegister' => Route::has('register'),
+		'laravelVersion' => Application::VERSION,
+		'phpVersion' => PHP_VERSION,
+	]);
+})->name('index');
--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-// SET main domain to the route group below
-// Route::domain(config('app.url'))->group(function () {
-	Route::get('/', function () {
-		return Inertia::render('Welcome', [
-			'canLogin' => Route::has('login'),
-			'canRegister' => Route::has('register'),
-			'laravelVersion' => Application::VERSION,
-			'phpVersion' => PHP_VERSION,
-		]);
-	})->name('index');
+// auth routes
+require __DIR__.'/auth/index.php';
 
-	require __DIR__.'/auth/index.php';
-// });
+// public routes
+require __DIR__.'/public/index.php';
 
-// SET subdomain admin to the route group below
-// Route::domain('admin.'.config('app.url'))->middleware(['auth'])->group(function () {
-// 	require __DIR__.'/admin/user.php';
-// 	require __DIR__.'/admin/role.php';
-// 	require __DIR__.'/admin/index.php';
-
-// 	require __DIR__.'/admin/category.php';
-// 	require __DIR__.'/admin/item.php';
-// });
-
+// customer routes
 Route::middleware(['auth', 'customer'])->group(function () {
-  require __DIR__.'/customer/index.php';
+	require __DIR__.'/customer/index.php';
 });
 
-
+// admin routes
 Route::prefix('admin')->middleware(['auth'])->group(function () {
 	require __DIR__.'/admin/user.php';
 	require __DIR__.'/admin/role.php';
@@ -42,5 +23,5 @@ Route::prefix('admin')->middleware(['auth'])->group(function () {
 	require __DIR__.'/admin/item.php';
 });
 
+// components routes
 require __DIR__.'/components/index.php';
-
