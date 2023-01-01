25bfa2767e2f437b38cf92cacebba2bf6c689d4a
phojie rengel <phojrengel@gmail.com>
2023-01-01 23:16:17 +0800

    feat: add category public routes

--- app/Http/Controllers/Customer/CategoryController.php
+++ app/Http/Controllers/Customer/CategoryController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use Inertia\Inertia;
 
 class CategoryController
 {
+  public function index() {
+    // TODO: Add pagination
+    $categories = Category::get();
+
+    return Inertia::render('Customer/Category/Index', [
+      'products' => CategoryResource::collection($categories),
+    ]);
+  }
+
 	public function show(string $slug)
 	{
 		$category = Category::where('slug', $slug)->firstOrFail();
--- routes/public/category.php
+++ routes/public/category.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('categories')->group(function () {
 	Route::controller(CategoryController::class)->group(function () {
+    // index
+    Route::get('/', 'index')->name('categories.index');
+
     // show
     Route::get('/{slug}', 'show')->name('categories.show');
 	});
