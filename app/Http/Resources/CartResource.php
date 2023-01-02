7eccc156443dbef0e2047e4525bc67fa61d2d8fb
phojie rengel <phojrengel@gmail.com>
2023-01-02 16:33:28 +0800

    style: update name casing

--- app/Http/Resources/CartResource.php
+++ app/Http/Resources/CartResource.php
@@ -10,11 +10,11 @@ class CartResource extends JsonResource
 	{
 		return [
 			'id' => $this->id,
-			'user_id' => $this->user_id,
-			'variant_id' => $this->variant_id,
+			'userId' => $this->user_id,
+			'variantId' => $this->variant_id,
 			'quantity' => $this->quantity,
-			'created_at' => $this->created_at,
-			'updated_at' => $this->updated_at,
+			'createdAt' => $this->created_at,
+			'updatedAt' => $this->updated_at,
 
 			// relations
 			'variant' => new VariantResource($this->whenLoaded('variant')),
