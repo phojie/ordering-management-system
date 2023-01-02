35bc1e44f94802e4810e3ba208ea8c56811acdc1
phojie rengel <phojrengel@gmail.com>
2023-01-02 16:12:47 +0800

    style: set id numbers to string

--- resources/js/types/cart.ts
+++ resources/js/types/cart.ts
@@ -2,9 +2,9 @@ import type { Variant } from './variant'
 import type { Product } from './product'
 
 export interface Cart {
-  id: number
-  user_id: number
-  variant_id: number
+  id: string
+  user_id: string
+  variant_id: string
   quantity: number
   created_at?: string
   updated_at?: string
