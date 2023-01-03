7ad4d689947771bbd283c71f19c2ab5750142872
phojie rengel <phojrengel@gmail.com>
2023-01-04 04:29:04 +0800

    style: add header to product index

--- resources/js/pages/Customer/Products/Index.vue
+++ resources/js/pages/Customer/Products/Index.vue
@@ -30,13 +30,24 @@ watch(search, _.debounce((value: string) => {
 
 <template>
   <div class="flex flex-col px-4 py-10 mx-auto space-y-8 overflow-hidden max-w-7xl sm:px-6 lg:px-8">
-    <div class="flex justify-end">
-      <InputSearch
-        v-model="search"
-        class="w-full lg:max-w-md"
-        :processing="processing"
-        placeholder="Search by product name"
-      />
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+          Shop our products
+        </h2>
+        <p class="mt-4 text-base text-gray-500">
+          Browse our stores and find the best products for you.
+        </p>
+      </div>
+
+      <div class="flex justify-end">
+        <InputSearch
+          v-model="search"
+          class="w-full lg:max-w-md"
+          :processing="processing"
+          placeholder="Search by product name"
+        />
+      </div>
     </div>
 
     <div class="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 lg:gap-x-8">
