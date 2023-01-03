8ceaa1cf4c6ad112965fbb496e98c4ad9a1cc703
phojie rengel <phojrengel@gmail.com>
2023-01-04 04:25:54 +0800

    style: update button type

--- resources/js/components/App/AppFooter.vue
+++ resources/js/components/App/AppFooter.vue
@@ -96,7 +96,7 @@ const footerNavigation = {
               >
               <div class="flex-shrink-0 ml-4">
                 <button
-                  type="submit"
+                  type="button"
                   class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
                 >
                   Sign up
