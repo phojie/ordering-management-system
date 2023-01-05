716aaf25869705925b00c96a178d2fa7de529b85
phojie rengel <phojrengel@gmail.com>
2023-01-05 13:14:38 +0800

    fix: remove ide-helper

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
 		/*
 		 * Package Service Providers...
 		 */
-		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+		// Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
 		/*
 		 * Application Service Providers...
