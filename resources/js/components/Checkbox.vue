fd4e2c5608e54ec22f0ce2347cc0e20b5be50b65
phojie rengel <phojrengel@gmail.com>
2023-01-05 13:37:36 +0800

    fix: some bugs

--- resources/js/components/Checkbox.vue
+++ resources/js/components/Checkbox.vue
@@ -1,30 +1,32 @@
 <script setup>
-import { computed } from 'vue';
-
-const emit = defineEmits(['update:checked']);
+import { computed } from 'vue'
 
 const props = defineProps({
-    checked: {
-        type: [Array, Boolean],
-        default: false,
-    },
-    value: {
-        default: null,
-    },
-});
+  checked: {
+    type: [Array, Boolean],
+    default: false,
+  },
+  value: {
+    default: null,
+  },
+})
+
+const emit = defineEmits(['update:checked'])
 
 const proxyChecked = computed({
-    get() {
-        return props.checked;
-    },
+  get() {
+    return props.checked
+  },
 
-    set(val) {
-        emit("update:checked", val);
-    },
-});
+  set(val) {
+    emit('update:checked', val)
+  },
+})
 </script>
 
 <template>
-    <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50">
+  <input
+    v-model="proxyChecked" type="checkbox" :value="value"
+    class="rounded border-gray-300 text-primary-600 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50"
+  >
 </template>
