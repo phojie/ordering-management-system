7cc225420df5e433d88cc8c9b24d3ec3640ae746
phojie rengel <phojrengel@gmail.com>
2023-02-12 03:57:55 +0800

    perf: remove unuse lines

--- routes/channels.php
+++ routes/channels.php
@@ -16,19 +16,4 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-//     return (int) $user->id === (int) $id;
-// });
-
-// Broadcast::channel('orders.{orderId}', function (User $user, int $orderId) {
-//   // return $user->id === Order::findOrNew($orderId)->user_id;
-//   return true;
-// });
-
-// Broadcast::channel('user.{userId}', function (User $user, int $userId) {
-//   return true;
-// });
-
 Broadcast::channel('orders.{userId}', OrderChannel::class);
-
-
