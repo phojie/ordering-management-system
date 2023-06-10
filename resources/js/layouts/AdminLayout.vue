82edf780e8209811fe3aa23b4ffb544d837b49e8
Phojie Rengel <phojrengel@gmail.com>
2023-06-10 08:36:29 +0800

    style: format

--- resources/js/layouts/AdminLayout.vue
+++ resources/js/layouts/AdminLayout.vue
@@ -21,7 +21,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="h-screen ">
+  <div class="h-screen">
     <AdminSideBar />
     <div class="flex flex-col h-full pb-24 overflow-auto bg-gray-100 lg:pl-64">
       <AdminHeader />
